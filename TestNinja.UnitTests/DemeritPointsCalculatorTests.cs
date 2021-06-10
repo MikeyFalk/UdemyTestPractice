75f32085cef28aea16272363652643f418f1eb34
Mike Falk <falk478@yahoo.com>
2021-06-10 11:24:37 -0700

    Added tests to fulfill requirement for exercises to practice testing Stack and DemeritPointsCalculator classes.

---
+++ TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,80 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class DemeritPointsCalculatorTests
+    {
+        [Test]
+        public void CalculateDemeritPoints_WhenCalledWithSpeedAboveMax_ThrowsError()
+        {
+            var pointsCalculator = new DemeritPointsCalculator();
+
+
+            Assert.That(() => pointsCalculator.CalculateDemeritPoints(301),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+
+        }
+
+        [Test]
+        public void CalculateDemeritPoints_WhenCalledWithSpeedBelowMin_ThrowsError()
+        {
+            var pointsCalculator = new DemeritPointsCalculator();            
+
+            Assert.That(() => pointsCalculator.CalculateDemeritPoints(-1),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+
+        }
+
+        //This is a parameterized test to handle the same cases as the two tests above.
+
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+
+        public void CalculateDemeritPoints_WhenOutofRangeSpeedIsEntered_ThrowsError(int crazySpeed)
+        {
+            var calculator = new DemeritPointsCalculator();
+
+            Assert.That(() => calculator.CalculateDemeritPoints(crazySpeed),
+                Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0, 0)]
+        [TestCase(64, 0)]
+        [TestCase(65, 0)]
+        [TestCase(69, 0)]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+
+        public void CalculateDemeritPoints_WhenCalledWithSpeedLimit_ReturnsDemeritPoints( int speedLimt, int demeritPoints)
+        {
+            var pointsCalculator = new DemeritPointsCalculator();
+
+            var result = pointsCalculator.CalculateDemeritPoints(speedLimt);
+
+            Assert.That(result, Is.EqualTo(demeritPoints));
+        }
+
+
+    // This is a method that tests a single parameter the test above is better because it tests multiple parameters with a single method
+      /* [Test]
+        public void CalculateDemeritPoints_WhenCalledWithSpeedLimitPlusFive_ReturnsOneDemeritPoint()
+        {
+            var pointsCalculator = new DemeritPointsCalculator();
+            
+            var result = pointsCalculator.CalculateDemeritPoints(70);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+      */
+
+    }
+}
---
+++ TestNinja.UnitTests/StackTests.cs
@@ -0,0 +1,121 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    class StackTests
+    {
+        [Test]
+        public void Push_WhenCalledWithNullObject_ThrowsError()
+        {
+            var stack = new Fundamentals.Stack<string>();
+
+            Assert.That(() => stack.Push(null), Throws.ArgumentNullException);
+
+
+        }
+        [Test]
+        public void Push_WhenCalled_AddsObjectToStack()
+        {
+            var stack = new Stack<string>();
+            
+            stack.Push("a");
+
+            Assert.That(stack.Count, Is.EqualTo(1));
+        }
+
+        // There could be a bug in the implementation if Count is hard-coded. The following test will confirm if the list is empty 
+        [Test]
+        public void Count_EmptyStack_ReturnsZero()
+        {
+            var stack = new Stack<string>();
+        
+            Assert.That(stack.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Pop_WhenCalledWithEmptyList_ThrowsInvalidOperationException()
+        {
+            var stack = new Stack<string>();
+
+            Assert.That(() => stack.Pop(), Throws.InvalidOperationException);
+        }
+        [Test]
+        public void Pop_WhenCalled_RemovesObjectFromList()
+        {
+            // Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            // Act
+            stack.Pop();
+
+            // Assert
+            Assert.That(stack.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Pop_WhenCalled_ReturnsObjectFromTop()
+        {
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            var result = stack.Pop();
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+
+        [Test]
+        public void Peek_WhenCalledWithEmptyStack_ThrowsInvalidOperationException()
+        {
+            var stack = new Stack<String>();
+
+            Assert.That(() => stack.Peek(), Throws.InvalidOperationException);
+
+        }
+        [Test]
+        public void Peek_WhenCalled__ReturnsObject()
+        {
+            // Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+
+            // Act
+            var result = stack.Peek();
+            
+            // Assert
+            Assert.That(result, Is.EqualTo("c"));
+        }
+
+        [Test]
+
+        public void Peek_WhenCalled_DoesntRemoveObjectFromStack()
+        {
+            // Arrange
+            var stack = new Stack<string>();
+            stack.Push("a");
+            stack.Push("b");
+            stack.Push("c");
+            
+            // Act
+            stack.Peek();
+
+            // Assert
+            Assert.That(stack.Count, Is.EqualTo(3));
+
+
+
+        }
+    }
+}
